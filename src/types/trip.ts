27aea50e9081fd7ce8250970781bd4257f77073e
falcaodiogo <falcao.diogo@ua.pt>
2024-05-03 15:28:51 +0100

    Entities retored from the homework

---
+++ src/types/trip.ts
@@ -0,0 +1,29 @@
+import { Bus, BusReference } from "./bus";
+import { City, CityReference } from "./city";
+import { CurrencyParams } from "./currency";
+
+export interface Trip {
+  id: number;
+  bus: Bus;
+  departure: City;
+  departureTime: Date;
+  arrival: City;
+  arrivalTime: Date;
+  price: number;
+  freeSeats: number;
+}
+
+export type TripCreate = Omit<Trip, "id" | "bus" | "departure" | "arrival"> & {
+  bus: BusReference;
+  departure: CityReference;
+  arrival: CityReference;
+};
+
+export type TripReference = Pick<Trip, "id">;
+
+export type TripSearchParameters = CurrencyParams & {
+  departure?: number;
+  arrival?: number;
+  departureTime?: string;
+  seats?: number;
+};
