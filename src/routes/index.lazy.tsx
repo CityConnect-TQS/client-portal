3f1606506c2bd2eeef73f1ac260a86fedd222c80
falcaodiogo <falcao.diogo@ua.pt>
2024-05-03 15:29:09 +0100

    Updated index to reactQuery

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,14 +1,40 @@
-import { DatePicker, Select, SelectItem, Button } from "@nextui-org/react";
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  DatePicker,
+  Button,
+  Autocomplete,
+  AutocompleteItem,
+} from "@nextui-org/react";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { parseAbsoluteToLocal } from "@internationalized/date";
-import React from "react";
+import React, { createContext, useState } from "react";
 import { NavbarClient } from "@/components/navbar";
+import { useCookies } from "react-cookie";
+import { useQuery } from "@tanstack/react-query";
+import { City } from "@/types/city";
+import { getCities } from "@/service/cityService";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
 
-// eslint-disable-next-line react-refresh/only-export-components
+type a = {
+  departure: number;
+  setDeparture: (id: number) => void;
+  arrival: number;
+  departureTime: string;
+};
+
+export const TripSearchContext = createContext<a>({
+  departure: 0,
+  setDeparture: () => {},
+  arrival: 0,
+  departureTime: "",
+});
+
 export const staticCities = [
   { value: "1", label: "Aveiro" },
   { value: "2", label: "Ovar" },
@@ -19,6 +45,16 @@ export const staticCities = [
 function Index() {
   const [date, setDate] = React.useState(new Date());
   const formattedDate = date.toISOString();
+  const [cookies] = useCookies();
+
+  const [departure, setDeparture] = useState<number>(0);
+  const [arrival, setArrival] = useState<number>(0);
+  // const [departureTime, setDepartureTime] = useState<string>("");
+
+  const { data: cities } = useQuery<City[]>({
+    queryKey: ["cities"],
+    queryFn: () => getCities(),
+  });
 
   return (
     <div className="p-2 h-screen flex flex-col">
@@ -32,28 +68,46 @@ function Index() {
             Your endless journeys start here.
           </h2>
           <div className="flex flex-col items-center w-full space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4 text-center">
-            <Select
-              items={staticCities}
+            <Autocomplete
               label="From"
-              placeholder="Origin"
+              id="origin"
               className="max-w-xs"
-              isRequired
+              onSelectionChange={(id) =>
+                setDeparture(id ? parseInt(id.toString()) : 0)
+              }
             >
-              {(cities) => (
-                <SelectItem key={cities.value}>{cities.label}</SelectItem>
-              )}
-            </Select>
-            <Select
-              items={staticCities}
+              {cities
+                ? cities.map((city: City) => (
+                    <AutocompleteItem
+                      key={city.id}
+                      value={city.name}
+                      id={"departure" + city.id}
+                    >
+                      {city.name}
+                    </AutocompleteItem>
+                  ))
+                : []}
+            </Autocomplete>
+            <Autocomplete
               label="To"
-              placeholder="Destination"
+              id="destination"
               className="max-w-xs"
-              isRequired
+              onSelectionChange={(id) =>
+                setArrival(id ? parseInt(id.toString()) : 0)
+              }
             >
-              {(cities) => (
-                <SelectItem key={cities.value}>{cities.label}</SelectItem>
-              )}
-            </Select>
+              {cities
+                ? cities.map((city) => (
+                    <AutocompleteItem
+                      key={city.id}
+                      value={city.name}
+                      id={"arrival" + city.id}
+                    >
+                      {city.name}
+                    </AutocompleteItem>
+                  ))
+                : []}
+            </Autocomplete>
             <DatePicker
               label="Departure"
               className="max-w-xs"
@@ -62,7 +116,16 @@ function Index() {
               onChange={(date) => setDate(date.toDate())}
               isRequired
             />
-            <Button color="primary" size="lg" className="h-14">
+            <Button
+              color="primary"
+              size="lg"
+              className="h-14"
+              onClick={() => {
+                cookies.set("departure", departure.toString());
+                cookies.set("arrival", arrival.toString());
+                cookies.set("departureTime", formattedDate);
+              }}
+            >
               Search
             </Button>
           </div>
