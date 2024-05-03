f31e57379dba94a9744528d28aa02d24285e66c5
falcaodiogo <falcao.diogo@ua.pt>
2024-05-03 13:34:20 +0100

    Bun.lockb and added logo, nabar and themeSwitcher

---
+++ src/components/navbar.tsx
@@ -0,0 +1,36 @@
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, Link, Button} from "@nextui-org/react";
+import {useState} from "react";
+import { ThemeSwitcher } from "./themeSwitcher";
+
+export function NavbarClient() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <img src="/logo.svg" alt="CityConnect" className="h-8 mr-4 w-auto rounded" />
+          <p className="font-bold text-inherit">CityConnect</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      </NavbarContent>
+      <NavbarContent justify="end">
+      <ThemeSwitcher />
+        <NavbarItem className="hidden lg:flex ">
+          <Link href="#" className="text-primary">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
