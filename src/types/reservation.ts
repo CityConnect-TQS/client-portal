7a7c6ee037b1222be290b3e7669cf7444c628633
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-10 01:17:33 +0100

    Add seats dummy data

--- src/types/reservation.ts
+++ src/types/reservation.ts
@@ -1,12 +1,23 @@
 import { Trip, TripReference } from "./trip";
 import { User, UserReference } from "./user";
 
-export type Reservation = {
+export interface Reservation {
   id: number;
   seats: number;
   trip: Trip;
   user: User;
-};
+}
+
+export interface Seat {
+  id: string;
+  isEnabled: boolean;
+  isAlreadyReserved: boolean;
+}
+
+export interface Row {
+  id: number;
+  seats: Seat[];
+}
 
 export type ReservationCreate = Omit<Reservation, "id" | "trip" | "user"> & {
   trip: TripReference;
