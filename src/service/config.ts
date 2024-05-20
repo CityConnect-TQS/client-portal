59be474d1fdbb01e388370e434b586860419a1ab
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-20 18:01:13 +0100

    Get host URL from environment

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,1 +1,2 @@
-export const BASE_API_URL = "http://api.localhost/api/public/";
+const VITE_HOST_URL = import.meta.env.VITE_HOST_URL as string;
+export const BASE_API_URL = `http://api.${VITE_HOST_URL}/api/public/`;
