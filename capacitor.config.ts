5e6b70b974c44ad7c00212d90415394f96b34cee
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-27 22:02:14 +0100

    Add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pt.ua.cityconnect',
+  appName: 'CityConnect',
+  webDir: 'dist'
+};
+
+export default config;
